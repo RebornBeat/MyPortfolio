d7e36347973cc9dce6997280b23ce28f440823fc
RebornBeat <christian.lfonts@gmail.com>
2021-01-12 19:26:34 -0400

    Info Container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,20 @@ function App() {
   return (
     <div id="container">
 		<div id="info_container">
+			<div className="name">
+				Christian Liz-Fonts
+			</div>
+			<div className="name">
+				Full-Stack Web Developer
+			</div>
+			<div id="contact_container">
+				<div className="contact_wrapper">
+					
+				</div>	
+				<div className="contact_wrapper">
+					
+				</div>						
+			</div>
 		</div>
     </div>
   );
