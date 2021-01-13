ad5d263ac2a83a0405a2e52beb1f959496b3ef8c
RebornBeat <christian.lfonts@gmail.com>
2021-01-13 09:44:35 -0400

    Django Model

---
+++ backend/projects/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    Title = models.CharField(max_length=120)
+    Description = models.CharField(max_length=120)
+    PhotoName = models.CharField(max_length=120)
+    Tags = models.JSONField()
