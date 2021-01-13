f0f3df5aebcc1c59548c34aa168233d03e6a3489
RebornBeat <christian.lfonts@gmail.com>
2021-01-12 22:23:39 -0400

    Contact Hover

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,67 @@
+import React, { Component } from 'react';
 import './App.css';
 import github from './github.svg'
+import blue_github from './blue_github.svg'
 import email from './email.svg'
+import blue_email from './blue_email.svg'
 
-function App() {
-  return (
-    <div id="container">
-		<div id="info_container">
-			<div className="name">
-				Christian Liz-Fonts
-			</div>
-			<div className="name">
-				Full-Stack Web Developer
-			</div>
-			<div id="contact_container">
-				<div className="contact_wrapper">
-					<img src={github} className="logo" alt="Logo" />
-					<span className="contact_text"> View Github </span>
-				</div>	
-				<div className="contact_wrapper">
-					<img src={email} className="logo" alt="Logo" />
-					<span className="contact_text"> Contact Me </span>
-				</div>						
+export default class App extends Component {
+	constructor(props) {
+	super(props);
+	this.state = { toggled: "n/a" };
+	}
+	
+	onHover (e) {
+		
+		if ( e.target.id === "github" ) {
+			this.setState({ toggled: "github" })
+			return
+		}
+		
+		if ( e.target.id === "email" ) {
+			this.setState({ toggled: "email" })
+			return
+		}
+		
+		this.setState({ toggled: "n/a" })
+		
+	}
+	
+	onExit (e) {
+		this.setState({ toggled: "n/a" })
+	}
+	
+	
+	componentDidUpdate() {
+		console.log("update", this.state.toggled)
+	}
+	
+	render () {
+	
+		return (
+		<div id="container">
+			<div id="info_container">
+				<div className="name">
+					Christian Liz-Fonts
+				</div>
+				<div className="name">
+					Full-Stack Web Developer
+				</div>
+				<div id="contact_container">
+					<div className="contact_wrapper" id="github" onMouseEnter= {this.onHover.bind(this)} onMouseLeave= {this.onExit.bind(this)} >
+						{ this.state.toggled != "github" && <img src={github} className="logo" alt="Logo" /> }
+						{ this.state.toggled === "github" && <img src={blue_github} className="logo" alt="Logo" /> }
+						<span className="contact_text"  > View Github </span>
+					</div>	
+					<div className="contact_wrapper" id="email" onMouseEnter= {this.onHover.bind(this)} onMouseLeave= {this.onExit.bind(this)} >
+						{ this.state.toggled != "email" && <img src={email} className="logo" alt="Logo" /> }
+						{ this.state.toggled === "email" && <img src={blue_email} className="logo" alt="Logo" /> }
+						<span className="contact_text" id="email" > Contact Me </span>
+					</div>						
+				</div>
 			</div>
 		</div>
-    </div>
-  );
+		);
+	}
 }
 
-export default App;
