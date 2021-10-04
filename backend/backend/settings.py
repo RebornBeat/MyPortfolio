7d25f500d73ecc597fa8fa08ffd6138925efd1e4
RebornBeat <christian.lfonts@gmail.com>
2021-10-04 09:29:37 -0400

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9@uv(!(a*5=h*(@@aljk(y_12q+m5ze-wbeco8=rf#9h5wc$@x'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
